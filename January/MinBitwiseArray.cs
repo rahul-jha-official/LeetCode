92496de1f577f696bec25dcab07bce36cdf11817
Rahul Jha <138975150+rahul-jha-official@users.noreply.github.com>
2026-01-20 18:23:25 +0530

    Create MinBitwiseArray.cs

---
+++ January/MinBitwiseArray.cs
@@ -0,0 +1,28 @@
+// LINK - https://leetcode.com/problems/construct-the-minimum-bitwise-array-i/description/
+
+
+public class Solution 
+{
+    public int[] MinBitwiseArray(IList<int> nums) 
+    {
+        var result = new int[nums.Count];
+
+        for (var i = 0; i < nums.Count; i++) 
+        {
+            var original = nums[i];
+            var candidate = -1;
+            for (var j = 1; j < original; j++) 
+            {
+                if ((j | (j + 1)) == original) 
+                {
+                    candidate = j;
+                    break;
+                }
+            }
+
+            result[i] = candidate;
+        }
+
+        return result;
+    }
+}
