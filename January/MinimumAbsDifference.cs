cddc9c1e8b2b0b9daf4434592b65544ffe0159b4
Rahul Jha <138975150+rahul-jha-official@users.noreply.github.com>
2026-01-30 22:02:13 +0530

    Update MinimumAbsDifference.cs

--- January/MinimumAbsDifference.cs
+++ January/MinimumAbsDifference.cs
@@ -27,15 +27,12 @@ public class Solution
 {
     public IList<IList<int>> MinimumAbsDifference(int[] arr) 
     {
-        // Sort
         Array.Sort(arr);
         
-        // Find Min
         var min = arr
                     .Select((e, i) => i == 0 ? int.MaxValue : e - arr[i - 1])
                     .Min();
                     
-        // Find Pairs
         return arr
                     .Select((e, i) => i == 0 || (e - arr[i - 1]) != min ? null : new List<int>() { arr[i - 1], e })
                     .Where(e => e != null)
